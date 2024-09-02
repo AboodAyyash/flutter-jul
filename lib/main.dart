d62107a05a29caa7755050cabeac6d74cdfd3069
AboodAyyash <46052083+AboodAyyash@users.noreply.github.com>
2024-09-02 20:33:44 +0300

    2-9-2024

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int counter = 20;
-
+  TextEditingController conName = TextEditingController();
+  TextEditingController conPassword = TextEditingController();
+  TextEditingController conPhone = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,281 +58,137 @@ class _MyHomePageState extends State<MyHomePage> {
               print("Hello");
             },
             icon: Icon(Icons.wifi),
-          )
+          ),
+            CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+              ),
+              
+            )
         ],
       ),
-      body: Center(
+      body: Container(
+        decoration: BoxDecoration(
+            /* image: DecorationImage(
+              image: NetworkImage(
+                "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+              ),
+              fit: BoxFit.cover), */
+            ),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                )
-              ],
+            SizedBox(
+              height: 10,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextField(
+                controller: conName,
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Name",
+                  labelText: "Name",
+                  labelStyle: TextStyle(fontSize: 50),
+                  hintStyle: TextStyle(fontSize: 30),
+                  border: OutlineInputBorder(),
                 ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                )
-              ],
+              ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                )
-              ],
+            SizedBox(
+              height: 10,
             ),
-            Wrap(
-              alignment: WrapAlignment.spaceEvenly,
-              children: [
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextField(
+                controller: conPassword,
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Password",
+                  labelText: "Password",
+                  labelStyle: TextStyle(fontSize: 50),
+                  hintStyle: TextStyle(fontSize: 30),
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: TextField(
+                controller: conPhone,
+                decoration: const InputDecoration(
+                  hintText: "Enter Your Phone Number",
+                  labelText: "Phone Number",
+                  labelStyle: TextStyle(fontSize: 50),
+                  hintStyle: TextStyle(fontSize: 30),
+                  border: OutlineInputBorder(),
+                ),
+                onChanged: (String value) {
+                  print(value);
+                  //call api
+                },
+                onSubmitted: (value) {
+                  print("onSubmitted");
+                },
+                onTap: () {
+                  print("Tapped");
+                },
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                print(conName.text.toString());
+                print(conPassword.text.toString());
+                print(conPhone.text.toString());
+
+                if (conName.text.toString().isEmpty ||
+                    conPassword.text.toString().isEmpty) {
+                  print("Fill Name And Pasword");
+                } else {
+                  print("Loggedin");
+                }
+              },
+              child: Text("Login"),
+            ),
+            TextButton(
+              onPressed: () {
+                conName.text = "Ali";
+                conPassword.text = "123456";
+                conPhone.text = "078";
+              },
+              child: Text("FillData"),
+            ),
+            Container(
+              height: 50,
+              margin: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                      "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
                     ),
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                ),
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                ),
-                InkWell(
-                  onTap: () {
-                    setState(() {
-                      print("B $counter ");
-                      counter += 1;
-                      print("A $counter ");
-                    });
-                  },
-                  child: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                    decoration: BoxDecoration(
-                      color: Colors.red,
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    child: Icon(Icons.add),
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    borderRadius: BorderRadius.circular(4),
-                  ),
-                  child: Text(
-                    "$counter",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                      backgroundColor: Colors.blue,
-                    ),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    setState(() {
-                      counter -= 1;
-                    });
-                  },
-                  icon: Icon(Icons.remove),
-                ),
-                ],
-            )
+                    fit: BoxFit.cover),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            /* CircleAvatar(
+              backgroundImage: NetworkImage(
+                "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+              ),
+              radius: 50,
+            ) */
+            /*  Image.network(
+              "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
+              width: 200,
+              height: 400,
+              fit: BoxFit.cover,
+            ) */
+            Image.asset(
+             "assets/images/images.jpeg",
+              width: 200,
+              height: 400,
+              fit: BoxFit.cover,
+            ) 
           ],
         ),
       ),
-      bottomNavigationBar: Container(
-        height: 90,
-        color: Colors.red,
-        width: 200,
-      ),
+     
     );
   }
 }
